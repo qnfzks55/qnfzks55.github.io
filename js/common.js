fff0e86430a2c48947c0cdd51b70cca4ce06c992
qnfzks55 <lmj5@naver.com>
2023-05-15 14:53:21 +0900

    포트폴리오 업데이트

--- js/common.js
+++ js/common.js
@@ -38,7 +38,8 @@ lmj.lmj_ui.prototype = (function () {
 			this.topAnimation();
 			this.scrollPosition();
 			this.portfolioScroll();
-			this.changeAgentMobile();
+			this.changeAgentMobile('cori');
+			this.changeAgentMobile('sl');
 		},
 		topAnimation: function () {
 			scrollTopBtn();
@@ -77,30 +78,37 @@ lmj.lmj_ui.prototype = (function () {
 				$(this).find('img').css("transform", "translate3d(0, 0, 0)");
 			});
 		},
-		changeAgentMobile: function () {
+		changeAgentMobile: function (type) {
 			var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
 			var deviceOs = null;
 			var androidLink = "https://play.google.com/store/apps/details?id=kr.richplanet.app.cori&hl=ko";
 			//var iosLink = "javascript:alert('앱스토어 등록 중입니다.')";
 			var iosLink = "https://itunes.apple.com/kr/app/id1476796492";
 			var downloadLink = "";
-			if (varUA.match('android') != null) {
-				//안드로이드 일때 처리
-				deviceOs = "android";
-				downloadLink = androidLink;
-			} else if (varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1
-				|| varUA.indexOf("ipod") > -1) {
-				//IOS 일때 처리
-				deviceOs = "ios";
-				downloadLink = iosLink;
-			} else {
-				//아이폰, 안드로이드 외 처리
-				downloadLink = "http://cori.pet";
-				$("#btn_app_link").on('click', function () {
-					alert('PC에서는 웹사이트로 이동합니다.\n해당 포트폴리오는 모바일에서 확인 바랍니다.');
-				});
+
+			if(type == 'cori'){
+				if (varUA.match('android') != null) { // 안드로이드 일때 처리
+					deviceOs = "android";
+					downloadLink = androidLink;
+				} else if (varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 || varUA.indexOf("ipod") > -1) {
+					//IOS 일때 처리
+					deviceOs = "ios";
+					downloadLink = iosLink;
+				} else {
+					//아이폰, 안드로이드 외 처리
+					downloadLink = "http://cori.pet";
+					$("#btn_app_link").on('click', function () {
+						alert('PC에서는 웹사이트로 이동합니다.\n해당 작업물은 모바일에서 확인 부탁드립니다.');
+					});
+				}
+				$("#btn_app_link").prop("href", downloadLink);
 			}
-			$("#btn_app_link").prop("href", downloadLink);
+			
+			if(type == 'sl') {
+				if (varUA.match('android') != null || varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 || varUA.indexOf("ipod") > -1) {
+					$("#btn_mo_link").prop("href", "./portfolio/sl_reser_event_mobile/page/index.html");
+				}
+			}
 		},
 		loadingUi: function () {
 			var loadingElement = document.createElement('div');
